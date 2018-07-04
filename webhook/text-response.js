a38bd180c09ba2ef68bdfd482eb6bc81b5c20370
GuanYQ0926 <guanyuqing0926@icloud.com>
2018-07-04 17:23:18 +0900

    QnA Maker

--- webhook/text-response.js
+++ webhook/text-response.js
@@ -11,18 +11,31 @@ module.exports = class TextResponse {
       this.context.log(`replyToken: ${replyToken}`)
       this.context.log(messages)
       this.context.log('send messages to 3rd server')
-      // request to thrid server
-      const url = `https://yuqingguan.top/text/${messages.text}`
-      axios.get(url)
+      // // request to thrid server
+      // const url = `https://yuqingguan.top/text/${messages.text}`
+      // axios.get(url)
+      //   .then(res => {
+      //     const replyText = res.data
+      //     const reply = {
+      //       type: 'text',
+      //       text: replyText
+      //     }
+      //     return this.client.replyMessage(replyToken, reply)
+      //   })
+      // request to Auzre QnA Maker
+      const url = 'https://hokkoriai-qna.azurewebsites.net/qnamaker/knowledgebases/7a05a644-aacc-4177-b3af-73f3d249fe8f/generateAnswer',
+        data = {question: messages.text},
+        config = {
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'EndpointKey 0c088b78-4811-4565-b452-d121ae9075b1'
+          },
+        }
+      axios.post(url, data, config)
         .then(res => {
-          const replyText = res.data
-          const reply = {
-            type: 'text',
-            text: replyText
-          }
-          return this.client.replyMessage(replyToken, reply)
+          this.context.log(res)
         })
-      // return this.client.replyMessage(replyToken, messages)
+        .catch(err => {this.context.log(`axios post error: ${err}`)})
     }
     else {
       return Promise.resolve(null)

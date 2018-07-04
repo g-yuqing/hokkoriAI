9774de99d4517794189748665ddba899a2efac11
GuanYQ0926 <guanyuqing0926@icloud.com>
2018-07-05 01:21:21 +0900

    audio test

--- webhook/audio-response.js
+++ webhook/audio-response.js
@@ -1,3 +1,5 @@
+const axios = require('axios')
+
 module.exports = class AudioResponse {
   constructor(lineClient, context, isDebug) {
     this.client = lineClient
@@ -7,12 +9,21 @@ module.exports = class AudioResponse {
   replyMessage(replyToken, message) {
     if(this.isDebug == 'false') {
       this.context.log('AudioResponse: send messages to 3rd server')
-      const url = 'https://yuqingguan.top/audio/'
+      const url = 'https://yuqingguan.top/audio/',
+        data = {question: message.text},
+        config = {
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'EndpointKey 0c088b78-4811-4565-b452-d121ae9075b1'
+          },
+        }
       this.client.getMessageContent(message.id)
         .then(stream => {
           this.context.log('content of stream')
-          this.context.log(typeof(stream))
-          this.context.log(stream)
+          axios.post(url, stream, config)
+            .then(res => {
+              this.context.log(res)
+            })
         })
     }
     else {

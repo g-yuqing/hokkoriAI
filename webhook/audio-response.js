34ce677945c2cded386b6561bfc107424e64f693
GuanYQ0926 <guanyuqing0926@icloud.com>
2018-07-05 23:38:56 +0900

    audio test

--- webhook/audio-response.js
+++ webhook/audio-response.js
@@ -70,7 +70,9 @@ module.exports = class AudioResponse {
     return this.client.getMessageContent(messageId)
       .then(stream => new Promise((resolve, reject) => {
         const writable = fs.createWriteStream(downloadPath)
-        stream.pipe(writable)
+        stream.on('data', chunk => {
+          chunk.pipe(writable)
+        })
         stream.on('end', () => resolve(downloadPath))
         stream.on('error', reject)
       }))

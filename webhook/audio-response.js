9156e00cdba5ba6398f6e2dac96b33b67b287815
GuanYQ0926 <guanyuqing0926@icloud.com>
2018-07-08 13:10:35 +0900

    audio

--- webhook/audio-response.js
+++ webhook/audio-response.js
@@ -1,7 +1,9 @@
-// const FormData = require('form-data')
+const FormData = require('form-data')
 const fs = require('fs')
 const path = require('path')
 const request = require('request')
+const axios = require('axios')
+
 
 module.exports = class AudioResponse {
   constructor(lineClient, context, isDebug) {
@@ -16,36 +18,46 @@ module.exports = class AudioResponse {
       this.downloadAudio(message.id, downloadPath)
         .then(() => {
           this.context.log('AudioResponse: file saved, send messages to 3rd server')
-          const formData = {
-            'file': fs.createReadStream(downloadPath),
-            // 'timeout': 180000
+          const form = new FormData()
+          form.append('file', fs.createReadStream(downloadPath))
+          const config = {
+            headers: form.getHeaders()
           }
-          this.context.log('init over')
-          request.post({url: url, formData: formData}, function(err, res, body) {
-            this.context.log('working?')
-            this.context.log(body)
-            if (!err && res.statusCode == 200) {
-              this.context.log('body')
-              this.context.log(body)
-              this.context.log(body.fussy, body.hungry, body.pain)
-              this.context.log('res')
+          axios.post(url, form, config)
+            .then(res => {
               this.context.log(res)
-              const reply = {
-                type: 'text',
-                text: 'processed'
-              }
-              return this.client.replyMessage(replyToken, reply)
-            }
-            else {
-              this.context.log('error')
-              this.context.log(`upload error: ${err}`)
-              const reply = {
-                type: 'text',
-                text: 'error happened'
-              }
-              return this.client.replyMessage(replyToken, reply)
-            }
-          })
+            })
+            .catch(err => {this.context.log(`axios post error: ${err}`)})
+          // const formData = {
+          //   'file': fs.createReadStream(downloadPath),
+          //   // 'timeout': 180000
+          // }
+          // this.context.log('init over')
+          // request.post({url: url, formData: formData}, function(err, res, body) {
+          //   this.context.log('working?')
+          //   this.context.log(body)
+          //   if (!err && res.statusCode == 200) {
+          //     this.context.log('body')
+          //     this.context.log(body)
+          //     this.context.log(body.fussy, body.hungry, body.pain)
+          //     this.context.log('res')
+          //     this.context.log(res)
+          //     const reply = {
+          //       type: 'text',
+          //       text: 'processed'
+          //     }
+          //     return this.client.replyMessage(replyToken, reply)
+          //   }
+          //   else {
+          //     this.context.log('error')
+          //     this.context.log(`upload error: ${err}`)
+          //     const reply = {
+          //       type: 'text',
+          //       text: 'error happened'
+          //     }
+          //     return this.client.replyMessage(replyToken, reply)
+          //   }
+          // })
         })
     }
     else {

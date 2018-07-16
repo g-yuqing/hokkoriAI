db5cc90197a3341ef184850988013910251c8f1f
GuanYQ0926 <guanyuqing0926@icloud.com>
2018-07-17 01:40:03 +0900

    audio test

--- webhook_dev/index.js
+++ webhook_dev/index.js
@@ -32,15 +32,13 @@ module.exports = function(context, req) {
     switch(event.type) {
     case 'message':
       switch(event.message.type) {
-      case 'text':
+      case 'audio':
+        return audioReply(event, 'label')
+      default:
         return client.replyMessage(event.replyToken, {
           type: 'text',
           text: '音声で入力してみてください'
         })
-      case 'audio':
-        return audioReply(event)
-      default:
-        return
       }
     case 'postback':
       return audioPostback(event)
@@ -77,29 +75,92 @@ module.exports = function(context, req) {
     //   return Promise.resolve(null)
     // }
   }
-
-  function audioReply(event) {
-    const reply = {
-      type: 'template',
-      altText: 'date pickers alt text',
-      template: {
-        type: 'buttons',
-        text: '赤ちゃんの誕生日を選んでください',
-        actions: [
-          { type: 'datetimepicker', label: 'date', data: 'DATE', mode: 'date' }
-        ],
-      },
+  function audioReply(event, type) {
+    let reply = {}
+    if(type === 'label') {
+      reply = {
+        type: 'template',
+        altText: 'label pickers alt text',
+        template: {
+          type: 'buttons',
+          text: '泣きの原因を選んでください',
+          actions: [
+            { type: 'message', label: '特に理由なし', data: 'FUSSY' },
+            { type: 'message', label: 'お腹が空いてる', data: 'HUNGRY' },
+            { type: 'message', label: 'どこか痛くしてる', data: 'PAIN' },
+            { type: 'message', label: 'そのほか', data: 'OTHER' }
+          ],
+        },
+      }
+    }
+    else if(type === 'birth') {
+      reply = {
+        type: 'template',
+        altText: 'date pickers alt text',
+        template: {
+          type: 'buttons',
+          text: '赤ちゃんの誕生日を選んでください',
+          actions: [
+            { type: 'datetimepicker', label: 'date', data: 'DATE', mode: 'date' }
+          ],
+        },
+      }
+    }
+    else if(type === 'confirm') {
+      const params = JSON.stringify(event.postback.params)
+      reply = {
+        type: 'template',
+        altText: 'confirm alt text',
+        template: {
+          type: 'confirm',
+          text: `${params}`,
+          actions: [
+            { label: 'Yes', type: 'message', text: 'はい!', data: 'YES' },
+            { label: 'No', type: 'message', text: 'いいえ!', data: 'NO' },
+            { label: 'DISCARD', type: 'message', text: 'やめる!', data: 'DISCARD' },
+          ],
+        },
+      }
+    }
+    else {
+      reply = {}
     }
     return client.replyMessage(event.replyToken, reply)
   }
   function audioPostback(event) {
-    let data = event.postback.data
-    if(data === 'DATE') {
-      const temp = JSON.stringify(event.postback.params)
-      return client.replyMessage(event.replyToken, {
-        type: 'text',
-        text: `post content: ${temp}`
-      })
+    const data = event.postback.data
+    if(data === 'FUSSY' || data === 'HUNGRY' || data === 'PAIN' || data === 'OTHER') {
+      return audioReply(event, 'birth')
+    }
+    else if (data === 'DATE') {
+      return audioReply(event, 'confirm')
+      // const temp = JSON.stringify(event.postback.params)
+      // return client.replyMessage(event.replyToken, {
+      //   type: 'text',
+      //   text: `post content: ${temp}`
+      // })
+    }
+    else if (data === 'NO') {
+      return audioReply(event, 'birth')
+    }
+    else if (data === 'DISCARD') {
+      return client.replyMessage(
+        event.replyToken, {
+          type: 'text',
+          text: 'データの保存をやめました、ありがとうございます！'
+        }
+      )
+    }
+    else if (data === 'YES') {
+      return client.replyMessage(
+        event.replyToken, {
+          type: 'text',
+          text: 'データを保存します、ありがとうございます！'
+        }
+      )
+    }
+    else {
+      return
     }
   }
 }

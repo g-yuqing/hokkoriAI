a85a18af54e01ecac5f730acb9c8d755d90b86dd
GuanYQ0926 <guanyuqing0926@icloud.com>
2018-09-05 18:51:00 +0900

    webhook-dev

--- webhook_dev/index.js
+++ webhook_dev/index.js
@@ -60,6 +60,7 @@ module.exports = function(context, req) {
     }
   }
   function audioReply(event, type) {
+    context.log('in audioReply', type)
     let reply = {}
     if(type === 'gender') {
       reply = {
@@ -76,7 +77,7 @@ module.exports = function(context, req) {
       }
     }
     else if(type === 'age') {
-      // info.gender = true
+      info.gender = true
       reply = {
         type: 'template',
         altText: 'age pickers alt text',
@@ -94,7 +95,7 @@ module.exports = function(context, req) {
       }
     }
     else if(type === 'confirm') {
-      // info.age = true
+      info.age = true
       const params = event.postback.params
       context.log(`confirm: ${params}`)
       reply = {
